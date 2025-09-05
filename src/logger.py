389e518a2509182c4ed6848b0d41a596c7bccd40
Pras-20 <prasannakumarpk2023@gmail.com>
2025-09-05 21:17:44 +0300

    created components pipline files implemented logging and custom exception handling

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import logging 
+import os 
+from datetime import datetime
+
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+    level=logging.INFO,
+)
+
+# logging.info("program started")
+# logging.info("this is warning")
+# logging.info("this is an error")
+ 
+
+if __name__=="__main__":
+    logging.info("logging has started")
