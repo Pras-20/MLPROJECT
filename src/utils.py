222033ddf01ad8452dd976d5a684dc2840186d78
Pras-20 <prasannakumarpk2023@gmail.com>
2025-09-12 22:10:31 +0300

    completed proper prediction pipline with html frontend

--- src/utils.py
+++ src/utils.py
@@ -57,3 +57,13 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,params):
 
         raise CustomException(e,sys)
     
+
+def load_object(file_path):
+
+    try:
+        with open(file_path,"rb") as obj:
+            return dill.load(obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+    
